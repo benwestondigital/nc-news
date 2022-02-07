ba702829d421d3bdda6513e53ac8c1cdc121a22d
Ben <benwestondigital@gmail.com>
2022-02-07 15:16:30 +0000

    add Header component and css

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <div className="Header">
+            <div className="Header__box Header__logo">Logo</div>
+            <h1>NC News</h1>
+            <div className="Header__box Header__user">User</div>
+        </div>
+    )
+}
+
