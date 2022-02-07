4f862b1bea00dbf35f01d51752f3dbf2219a89d0
Ben <benwestondigital@gmail.com>
2022-02-07 17:08:18 +0000

    add getTopics api call and mapped topics to form in Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,25 @@
 import '../css/Nav.css';
+import { getTopics } from '../utils/api';
+import { useState, useEffect } from 'react';
 
 const Nav = () => {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    return getTopics().then(res => {
+      setTopics(res);
+    });
+  }, []);
+
   return (
     <div className="Nav">
       <form>
         <label htmlFor="topics">Topics:</label>
-        <select id="topics"></select>
+        <select id="topics">
+          {topics.map(topic => {
+            return <option key={topic.slug}>{topic.slug}</option>;
+          })}
+        </select>
       </form>
       <form>
         <label htmlFor="sortby">Sort Articles By:</label>
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,8 @@ import axios from 'axios';
 const newsApi = axios.create({
     baseURL: 'https://ben-reddit-project.herokuapp.com/api'
   });
+
+  export async function getTopics() {
+      const topics = (await newsApi.get('/topics')).data.topics;
+      return topics;
+  }
