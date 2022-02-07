f0e2d0bc176a1afc53bfd195aeb7f35f6a17b515
Ben <benwestondigital@gmail.com>
2022-02-07 15:45:37 +0000

    add vote arrows from fontawesome

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,11 +1,10 @@
 import '../css/Votes.css';
 
 const Votes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div className="Votes">
+    <i class="fas fa-arrow-circle-up"></i>
+    <i class="fas fa-arrow-circle-down"></i>
+  </div>;
 };
 
 export default Votes;
