110a40c2bf9bac7469bf8f1cdea930fdf879edd8
Ben <benwestondigital@gmail.com>
2022-02-08 13:14:35 +0000

    added grid areas for articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,17 +4,16 @@ import { stringFormat, dateTimeFormat } from '../utils/utils';
 const Article = ({ data }) => {
   const { author, body, comment_count, created_at, title, topic, votes } = data;
   const {date, time} = dateTimeFormat(created_at);
-  console.log(date, time);
 
   return (
     <div className="Article">
-      <Votes votes={votes}/>
-      <p>{author}</p>
-      <p>{stringFormat(topic)}</p>
-      <p>{title}</p>
-      <p>{body.slice(0,100)}...</p>
-      <p>{comment_count} Comments</p>
-      <p>{date} {time}</p>
+      <p className='Article__author'>{author}</p>
+      <p className='Article__topic'>{stringFormat(topic)}</p>
+      <p className='Article__title'>{title}</p>
+      <p className='Article__body'>{body.slice(0,100)}...</p>
+      <p className='Article__commentcount'>{comment_count} Comments</p>
+      <p className='Article__datetime'>{date} {time}</p>
+      <Votes className='Article__votes' votes={votes}/>
     </div>
   );
 };
