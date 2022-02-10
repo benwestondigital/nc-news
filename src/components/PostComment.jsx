60cc1f6fdc9066949f113c03d6880b5b8264854f
Ben <benwestondigital@gmail.com>
2022-02-10 12:04:58 +0000

    add User context and implement on PostComment

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,12 +1,34 @@
+import { useState, useContext } from 'react';
+import { postComment } from '../utils/api';
+import { UserContext } from '../contexts/User';
 
-const PostComment = () => {
-    return (
-        <form>
-        <label htmlFor="addComment">Comment:</label>
-        <input placeholder="Leave a comment" id="addComment"></input>
-        <button>Add Comment</button>
-      </form>
-    );
+const PostComment = ({ article_id, setComments }) => {
+  const [input, setInput] = useState('');
+  const { user } = useContext(UserContext);
+
+  const handleInput = e => {
+    setInput(e.target.value);
+
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    postComment(article_id, user, input);
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="addComment">Comment:</label>
+      <input
+        onChange={handleInput}
+        value={input}
+        placeholder="Leave a comment"
+        id="addComment"
+      ></input>
+      <button>Add Comment</button>
+    </form>
+  );
 };
 
 export default PostComment;
