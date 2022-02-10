cf52c135883865660340e3773fcaab4e8ff7b4ab
Ben <benwestondigital@gmail.com>
2022-02-10 12:37:21 +0000

    add conditional rendering for comment delete button

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,19 +1,27 @@
+import { UserContext } from '../contexts/User';
+import { useContext } from 'react';
 import '../css/Comment.css';
 import { dateTimeFormat } from '../utils/utils';
 
-const Comment = ({data}) => {
-    const {created_at, author, body} = data;
-    const { date, time } = dateTimeFormat(created_at);
+const Comment = ({ data }) => {
+  const { user } = useContext(UserContext);
+  const { created_at, author, body } = data;
+  const { date, time } = dateTimeFormat(created_at);
 
-    return (
-        <div className='Comment'>
-            <p className="Comment__author">{author}</p>
-            <p className='Comment__body'>{body}</p>
-            <p className='Comment__datetime'>{date} {time}</p>
-            {/* if user === author then show delete button */}
-            <p className='Comment__delete'>Delete</p>
-        </div>
-    );
+  const renderDelete = () => author === user;
+
+  console.log(renderDelete());
+
+  return (
+    <div className="Comment">
+      <p className="Comment__author">{author}</p>
+      <p className="Comment__body">{body}</p>
+      <p className="Comment__datetime">
+        {date} {time}
+      </p>
+      {renderDelete() && <p className="Comment__delete">Delete</p>}
+    </div>
+  );
 };
 
 export default Comment;
