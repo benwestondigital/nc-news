0657cdf0773919f6da37d99b29e6315ae7ca076f
Ben <benwestondigital@gmail.com>
2022-02-11 11:34:47 +0000

    add error page and route

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+const Error = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <p>This is embarrassing... we couldn't find your page.</p>
+        </div>
+    );
+};
+
+export default Error;
