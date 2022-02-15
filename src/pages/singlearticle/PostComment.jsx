e56a570b4715c2461552676ba864271b071491fc
Ben <benwestondigital@gmail.com>
2022-02-15 18:51:01 +0000

    improve postcomment component styling

--- src/pages/singlearticle/PostComment.jsx
+++ src/pages/singlearticle/PostComment.jsx
@@ -1,17 +1,24 @@
+import './css/PostComment.css';
 import { UserContext } from '../../common/contexts/User';
 import { useState, useContext } from 'react';
 import { postComment } from '../../common/utils/api';
 
 const PostComment = ({ article_id, setComments }) => {
   const [input, setInput] = useState('');
+  const [warning, setWarning] = useState(false);
   const { user } = useContext(UserContext);
 
-  const handleInput = ({target:{value}}) => {
+  const handleInput = ({ target: { value } }) => {
+    setWarning(false);
     setInput(value);
   };
 
   const handleSubmit = async e => {
+    console.dir(e);
     e.preventDefault();
+    if (!input) {
+      return setWarning(true);
+    }
     const newComment = await postComment(article_id, user, input);
     setComments(currComments => {
       return [newComment, ...currComments];
@@ -20,17 +27,28 @@ const PostComment = ({ article_id, setComments }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="addComment">Comment:</label>
-      <input
-        onChange={handleInput}
-        value={input}
-        placeholder="Leave a comment"
-        id="addComment"
-        required
-      ></input>
-      <button type="button">Add Comment</button>
-    </form>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form__commentbox">
+          <label htmlFor="addComment">Comment:</label>
+          <textarea
+            onChange={handleInput}
+            value={input}
+            placeholder="Leave a comment"
+            id="addComment"
+          ></textarea>
+        </div>
+        <button
+          className={`form__button ${
+            warning ? 'form__button--inactive' : 'form__button--active'
+          }`}
+          type="submit"
+        >
+          Add Comment
+        </button>
+      </form>
+      <div className="form__warning">{warning && <p>Please enter a comment.</p>}</div>
+    </>
   );
 };
 
